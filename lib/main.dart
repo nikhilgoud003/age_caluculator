1dba0daf7a3b2589746b000f4229b497833350df
nikhilgoud003 <nikhilgoud2001@gmail.com>
2025-02-24 18:52:03 -0500

    Added the counter and the app basic design

--- lib/main.dart
+++ lib/main.dart
@@ -32,3 +32,40 @@ void setupWindow() {
     });
   }
 }
+
+class Counter with ChangeNotifier {
+  int value = 0;
+
+  void increment() {
+    value += 1;
+    notifyListeners();
+  }
+
+  void decrement() {
+    if (value > 0) {
+      value -= 1;
+    }
+    notifyListeners();
+  }
+
+  void setvalue(double val) {
+    value = val.toInt();
+    notifyListeners();
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Age Calculator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      // home: const MyHomePage(),
+    );
+  }
+}
